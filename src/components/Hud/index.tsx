20f4c1f2bcb2652e98d2c4db4f00355180613075
Nick Fausti <ncfausti@gmail.com>
2021-09-16 22:15:52 -0400

    Initial commit on dev.

---
+++ src/components/Hud/index.tsx
@@ -0,0 +1,32 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
+import React from 'react';
+
+export default function Hud() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+      <div className="w-full text-centerrelative py-3 sm:max-w-xl sm:mx-auto">
+        <div className="w-full bg-cyan-900 p-6 rounded-full">
+          <div className="w-full bg-cyan-400 p-4 rounded-full">
+            <div className="flex justify-between w-full bg-cyan-100 rounded-full">
+              <div className="w-36 bg-white p-6 rounded-full text-center">
+                0:45
+                <br />
+                Time Elapsed
+              </div>
+              <div className="w-24 bg-white p-6 rounded-full text-center -mb-6 mt-12">
+                <div className="text-2xl">75</div>
+                Score
+              </div>
+              <div className="w-36 bg-white p-6 rounded-full text-center">
+                114 WPM
+                <br />
+                Talk
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
