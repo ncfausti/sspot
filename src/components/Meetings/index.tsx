360753ae4571a51480a2204ed941a3eacf2b0305
Nick Fausti <ncfausti@gmail.com>
2021-11-04 23:21:57 -0400

    Add date and time styles.

--- src/components/Meetings/index.tsx
+++ src/components/Meetings/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/forbid-prop-types */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { CogIcon } from '@heroicons/react/solid';
 import log from 'electron-log';
 
@@ -38,12 +38,31 @@ export default function Meetings() {
       `top=80,left=200,frame=false,transparent=true, backgroundColor=#00000000`
     );
   }
-  const date = new Date();
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => setTime(new Date()), 1000);
+    log.info('updating time now');
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const dateStyle = new Intl.DateTimeFormat('en', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+
+  const timeStyle = new Intl.DateTimeFormat('en', {
+    timeStyle: 'short',
+  });
+
   return (
     <div className="flex flex-grow flex-col bg-red-100 min-h-screen content-center">
       <div className="flex flex-grow bg-blue-100 flex-wrap justify-between">
-        <div className="bg-blue-300">{date.toLocaleDateString()}</div>
-        <div className="bg-red-300">{date.toLocaleTimeString()}</div>
+        <div className="bg-blue-300">{dateStyle.format(time)}</div>
+        <div className="bg-red-300">{timeStyle.format(time)}</div>
       </div>
       <div className="flex flex-grow m-8">
         <button
@@ -58,7 +77,9 @@ export default function Meetings() {
         </button>
       </div>
       <div className="flex flex-grow flex-wrap justify-between">
-        <div className="bg-blue-300">SaleSpot LOGO HERE</div>
+        <div className="bg-blue-300">
+          SaleSpot LOGO HERE <span>⌘ + Y to toggle </span>
+        </div>
         <div className="bg-red-300">
           <CogIcon className="h-5 w-5 cursor-pointer" />
         </div>
