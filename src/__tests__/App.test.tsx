a2350604d64461322dfbabd589ff55545800795b
Nick Fausti <nick@smile-ml.com>
2021-12-18 04:48:01 -0600

    Add arm pymouse binary and test stubs for better server/mouseListener management.

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -5,8 +5,35 @@ import { act } from 'react-dom/test-utils';
 import App from '../App';
 
 describe('App', () => {
-  // it('should render', () => {
-  //   expect(render(<App />)).toBeTruthy();
-  // });
-  // expect(true).toBeTruthy();
+  it('should start the ws_server from main via ipc call', () => {
+    expect(1).toEqual(2);
+  });
+
+  // since we can now reset the state of the server easily,
+  // we should create the server on app start, then reset it
+  // when the user clicks Launch SaleSpot, eliminating the need for
+  // the loading screen prior to the HUD starting
+  it('should start the ws_server from main via ipc call', () => {
+    expect(1).toEqual(2);
+  });
+
+  it('should render', () => {
+    expect(render(<App />)).toBeTruthy();
+  });
+
+  it('should render with snapshot', () => {
+    expect(render(<App />)).toMatchSnapshot();
+  });
+
+  it('should start the python server', () => {
+    expect(1).toEqual(2);
+  });
+
+  it('should start the pymouse listener', () => {
+    expect(1).toEqual(2);
+  });
+
+  it('should guarantee that pymouse listener can only be registered once', () => {
+    expect(1).toEqual(2);
+  });
 });
