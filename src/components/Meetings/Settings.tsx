aa9f097f89cc0fde21cfafe9f098699305621175
Nick Fausti <ncfausti@users.noreply.github.com>
2021-12-22 15:15:43 -0600

    Update to v0.7.6

--- src/components/Meetings/Settings.tsx
+++ src/components/Meetings/Settings.tsx
@@ -93,7 +93,7 @@ export default function Settings(props) {
         >
           Quit
         </button>
-        <div className="text-xxs float-left ml-8 text-gray-300">v0.7.5</div>
+        <div className="text-xxs float-left ml-8 text-gray-300">v0.7.6</div>
       </div>
     </fieldset>
   );
