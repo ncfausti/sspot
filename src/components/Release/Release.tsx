ae3b30b0885206f93ebc445c2e906a663e42fb8b
Nick Fausti <nick@smile-ml.com>
2022-02-25 12:55:56 -0500

    Release v0.9.3.

--- src/components/Release/Release.tsx
+++ src/components/Release/Release.tsx
@@ -7,15 +7,15 @@ export default function Release() {
     <div className="p-5 h-screen bg-gray-100 text-black dark:bg-black dark:text-white">
 
       <div className="flex justify-between border-b">
-        <h1 className="text-xl">Release Notes <span className="text-sm">v0.9.2</span></h1>
+        <h1 className="text-xl">Release Notes <span className="text-sm">v0.9.3</span></h1>
         <XIcon
           className="cursor-pointer w-5 h-5 dark:text-white hover:dark:text-gray-400"
           onClick={() => window.close()}
         />
       </div>
       <ul className="p-5 list-disc text-sm">
-        <li>Update default alert message and durations.</li>
-        <li>Quit application menu on right-click of icon.</li>
+        <li>Update timer UI</li>
+        <li>Fix for multiple windows opening without user spotted</li>
       </ul>
     </div>
   );
